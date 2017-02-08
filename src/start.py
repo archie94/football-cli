9c9e3a359ec6fa5d273eabbfad0b822283496be3
archie94 <arkaprava94@gmail.com>
2017-02-09 00:35:19 +0530

    Add colored output

--- src/start.py
+++ src/start.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import datetime
+from util import colors
 
 def get_current_date():
 	now = datetime.datetime.now()
@@ -12,17 +13,24 @@ def get_html(url):
 def get_bsoup_object(htmltext):
 	return BeautifulSoup(htmltext, "lxml") # use lxml parser
 
+# print data in a tabular form
+def print_table(col1, col2, col3, col4):
+	for i in range(0, len(col1)):
+		print col1[i], '\t', col2[i], '\t\t\t', col3[i], '\t', col4[i]
+
 def matches_today(date, soup, htmltext):
 	soup = soup.find('section', attrs = {'class': 'matchdays-wrapper'})
 	current_day_matches = soup.find_all('section', attrs = {'class': 'matchday', 'data-day' : date})
 
 	for i in range(0, len(current_day_matches)):
+
 		header = current_day_matches[i].find('header', attrs = {'class': 'info-header'}).h3.text
-		print "Showing matches for ", header
+		print " Showing matches for ", header
 
 		tables = current_day_matches[i].find_all('table', attrs = {'class': 'matches'})
 		for j in range(0, len(tables)):
-			print "================================================================================="
+			out_color = colors.color[j%len(colors.color)]
+			print out_color, " ================================================================================="
 			tournament = tables[j].find('span', attrs = {'class': 'comp-title'})
 			if tournament is not None:
 				print tournament.text
@@ -30,6 +38,10 @@ def matches_today(date, soup, htmltext):
 			
 			print "Total ", len(fixtures), " fixtures for today"
 			
+			home_teams = []
+			away_teams = []
+			match_status = []
+			match_result = []
 			for fixture in fixtures:
 				status = fixture.find('td', attrs = {'class': 'status'}).span.text
 				vs = fixture.find('td', attrs = {'class': 'vs'}).div.text.strip('\n')
@@ -37,7 +49,12 @@ def matches_today(date, soup, htmltext):
 				team1, team2 = fixture.find_all('td', attrs = {'class': 'team'})
 				team1 = team1.div.span.text
 				team2 = team2.div.span.text
-				print status, '\t', team1, '\t', vs, '\t', team2
+				home_teams.append(team1)
+				away_teams.append(team2)
+				match_status.append(status)
+				match_result.append(vs)
+
+			print_table(match_status, home_teams, match_result, away_teams)
 
 def main():
 	date = get_current_date()
---
+++ src/util/colors.py
@@ -0,0 +1,12 @@
+#Console Colors
+RED 	= '\033[31m'  
+GREEN 	= '\033[32m'  
+ORANGE 	= '\033[33m'  
+BLUE 	= '\033[34m'  
+PURPLE 	= '\033[35m'  
+CYAN 	= '\033[36m'  
+GRAY 	= '\033[37m'
+BLACK 	= '\033[30m'
+END 	= '\033[0m'
+
+color 	= [BLUE,PURPLE,GREEN,CYAN,ORANGE,RED,END]
